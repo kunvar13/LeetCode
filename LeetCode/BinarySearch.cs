64cc0b580cc652272985d155acc7b7c450428daa
kunvar13 <kunvar13@gmail.com>
2022-03-24 14:45:39 -0600

    FindingPeak

--- LeetCode/BinarySearch.cs
+++ LeetCode/BinarySearch.cs
@@ -106,6 +106,30 @@ namespace LeetCode
 
             return left;
         }
-        
+
+
+        public int FindPeakElement(int[] nums)
+        {
+
+            int left = 0;
+            int right = nums.Length - 1;
+
+            while (left < right)
+            {
+                int mid = (right + left) / 2;
+                if (nums[mid] > nums[mid + 1])
+                {
+                    right = mid;
+                }
+                else
+                {
+                    left = mid + 1;
+                }
+            }
+
+            return left;
+
+        }
     }
+
 }
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -9,8 +9,8 @@ namespace LeetCode
             var result = new BinarySearch();
             int[] nums = { 1, 3, 5, 6 };
             //Console.WriteLine(result.BinaryAlgo(nums,10));
-            int Sqrt = 2;
-            Console.WriteLine (result.SearchInsert(nums, 7));
+            //int Sqrt = 2;
+            Console.WriteLine (result.FindPeakElement(nums));
         }
     }
 }
